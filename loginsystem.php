208cc7f463f2f07977d22adea71ddd449141baa1
Christian Jade Aguilar <christianjadeaguilar29@gmail.com>
2025-02-19 12:04:25 +0800

    new

--- loginsystem.php
+++ loginsystem.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login & Signup</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/loginstyle.css">
 </head>
 <body>
     <!--sign in form -->
@@ -51,8 +51,8 @@
                     <label for="password">Password</label>
                 </div>
                 <div class="input-group">
-                        <select id="roleDropdown" name="role" required>
-                            <option value="">Select Role</option>
+                        <select id="roleDropdown" name="role" placeholder="Password" required>
+                            <option value="" selected disabled hidden></option>
                             <option value="admin">Admin</option>
                             <option value="cashier">Cashier</option>
                         </select>
